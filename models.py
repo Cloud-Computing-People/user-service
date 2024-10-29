1938e8a6dcec099df6cf48ba5d28fe358737eaf7
Azam Khan <46198924+azamkhan99@users.noreply.github.com>
2024-10-28 21:52:43 -0400

    fixed links (#17)
    * fixed links
    
    * fixed links error
    
    ---------
    
    Co-authored-by: azamkhan99 <azan_khan99@outlook.com>

--- models.py
+++ models.py
@@ -1,27 +1,22 @@
 from pydantic import BaseModel, Field
 from typing import Annotated, Any, Optional, Dict, List
 
+
 class User(BaseModel):
-    id: Annotated[int, Field(
-        description="User ID",
-        examples=[1]
-    )]
-    username: Annotated[str, Field(
-        description="Username",
-        examples=["totally-not-darth-vader"]
-    )]
-    email: Annotated[str, Field(
-        description="User email",
-        examples=["darth_vader@empire.gov"]
-    )]
-    is_admin: Annotated[bool, Field(
-        description="User admin status",
-        examples=[False]
-    )]
+    id: Annotated[int, Field(description="User ID", examples=[1])]
+    username: Annotated[
+        str, Field(description="Username", examples=["totally-not-darth-vader"])
+    ]
+    email: Annotated[
+        str, Field(description="User email", examples=["darth_vader@empire.gov"])
+    ]
+    is_admin: Annotated[bool, Field(description="User admin status", examples=[False])]
+
 
 class UserLogin(BaseModel):
     username: str
 
+
 class ResponseModel(BaseModel):
     data: Any
-    links: Optional[Dict[str, Optional[str]]] = None
+    links: Optional[List[Dict[str, str]]] = None
